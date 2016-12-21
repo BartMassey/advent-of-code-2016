8f269ddc8b49322eb24ee838713f226659821e99
Bart Massey <bart@cs.pdx.edu>
2016-12-21 12:46:32 -0800

    finished code for day 19

--- day19/soln.rs
+++ day19/soln.rs
@@ -5,6 +5,9 @@
 
 // Advent of Code Day 19.
 
+#[cfg(debug_assertions)]
+const SHOW: bool = true;
+#[cfg(not(debug_assertions))]
 const SHOW: bool = false;
 
 extern crate aoc;
@@ -17,32 +20,40 @@ struct Elf {
 pub fn main() {
     let (part, args) = aoc::get_part_args();
     assert!(args.len() == 1);
-    let nelves = args[0].parse().expect("could not parse nelves");
-    let mut living_elves = nelves;
+    let nelves: usize = args[0].parse().expect("could not parse nelves");
     let mut elves = Vec::new();
     for i in 0..nelves {
         let elf = Elf { npresents: 1, next_elf: (i + 1) % nelves };
         elves.push(elf);
     };
     let mut cur_elf = 0;
-    while living_elves > 1 {
-        let mut victim_elf = cur_elf;
-        let mut victim_parent_elf = cur_elf;
+    let mut victim_parent =
         if part == 1 {
-            victim_elf = elves[victim_elf].next_elf;
+            0
         } else {
-            for _ in 0..living_elves>>1 {
-                victim_parent_elf = victim_elf;
-                victim_elf = elves[victim_elf].next_elf;
-            };
-            if SHOW {
-                print!("{} {}\n", cur_elf + 1, victim_elf + 1);
+            (nelves >> 1) - 1
+        };
+    let mut living_elves = nelves;
+    loop {
+        if SHOW {
+            print!("{} {}\n", cur_elf + 1, elves[victim_parent].next_elf + 1);
+        };
+        let victim_elf = elves[victim_parent].next_elf;
+        assert!(cur_elf != victim_elf);
+        elves[cur_elf].npresents += elves[victim_elf].npresents;
+        elves[victim_parent].next_elf = elves[victim_elf].next_elf;
+        living_elves -= 1;
+        if living_elves == 1 {
+            break;
+        }
+        cur_elf = elves[cur_elf].next_elf;
+        if part == 1 {
+            victim_parent = elves[victim_parent].next_elf;
+        } else {
+            if living_elves & 1 == 0 {
+                victim_parent = elves[victim_parent].next_elf;
             };
         };
-        elves[cur_elf].npresents += elves[victim_elf].npresents;
-        elves[victim_parent_elf].next_elf = elves[victim_elf].next_elf;
-        living_elves -= 1;
-        cur_elf = elves[cur_elf].next_elf;
     };
     assert!(elves[cur_elf].npresents == nelves);
     print!("{}\n", cur_elf + 1);
