469a9d6854f5ad8c9d6d3d6e89a437ccbcbd3a5a
Bart Massey <bart@cs.pdx.edu>
2016-12-21 01:31:56 -0800

    checkpointed day 19

--- day19/README.md
+++ day19/README.md
@@ -1,6 +1,13 @@
 # Advent of Code 2016: Day 19
 Copyright (c) 2016 Bart Massey
 
+Finally gave up on Rust after adventures to be detailed
+later and rewrote in Haskell.
+
+Rust: part 1 in 80ms. Haskell: part 1 in 1.4s. On the other
+hand, Haskell finished part 2 in about...1.4s. But currently
+gets a wrong answer.
+
 ---
 
 This program is licensed under the "MIT License".
---
+++ day19/soln.hs
@@ -0,0 +1,41 @@
+-- Copyright © 2016 Bart Massey
+
+-- This program is licensed under the "MIT License".
+-- Please see the file COPYING in this distribution
+-- for license terms.
+
+-- Advent of Code Day 19 in Haskell.
+
+import System.Environment
+import Data.Sequence as S hiding (length)
+
+del :: Int -> S.Seq Int -> S.Seq Int
+del i s
+    | i < 0 = error "index must be positive"
+    | i >= length s = error "index off end"
+    | otherwise = deleteAt i s
+
+exchange :: Int -> (Int -> Int -> Int) -> S.Seq Int -> (Int, [(Int, Int)])
+exchange posn nextElf elves
+    | length elves == 1 = (index elves 0, [])
+    | otherwise =
+        let victor_serial = index elves posn
+            victim = nextElf (length elves) posn
+            victim_serial = index elves victim
+            elves' = del victim elves
+            successor = (posn + 1) `mod` length elves in
+        let (result, serials) = exchange successor nextElf elves' in
+        (result, (victor_serial, victim_serial) : serials)
+
+main:: IO ()
+main = do
+  [partStr, countStr] <- getArgs
+  let part = read partStr :: Int
+  let n = read countStr :: Int
+  let elves = S.fromList [1..n]
+  let survivor =
+          case part of
+            1 -> exchange 0 (\n i -> (i + 1) `mod` n) elves
+            2 -> exchange 0 (\n i -> (i + (n `div` 2)) `mod` n) elves
+            _ -> error "unknown part"
+  print $ survivor
