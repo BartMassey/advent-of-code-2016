b1c5cfb17ed4d0adbd58d46cb4f88488bad0f4d6
Bart Massey <bart@cs.pdx.edu>
2016-12-27 15:46:44 -0800

    cleaned day 22; still need to lift A*

--- day22/README.md
+++ day22/README.md
@@ -6,19 +6,30 @@ Ok...
 Part 1 was a pretty straightforward exercise using the
 obvious quadratic algorithm.
 
-For part 2, I was getting all set up and started by
-transforming the board into a form that would be suitable
-for an A\* search. This probably would have worked, but upon
-printing the board I noticed that I could just count the
-answer out by hand...
+I started getting set up for part 2 by transforming the
+board into a form that would be suitable for an A\*
+search. This probably would have worked, but upon printing
+the board I noticed that I could just count the answer out
+by hand...
 
 For my input, it took 26 steps to get the blank just to the
 left of the goal data (dodging a wall), another 33 * 5 steps
 to move the goal data left to just before the target, and
 one more step to put it in place. Hooray really simple
-sliding tile puzzle. 
+sliding tile puzzle.
 
-I went back the next day and constructed the A\* search. 27ms.
+I went back the next day and constructed the A\* search
+anyway, just because. 27ms.
+
+## Usage
+
+Part 1:
+
+        cargo run 1 <input.txt
+
+Part 2:
+
+        cargo run --release 2 <input.txt
 
 ---
 
--- day22/soln.rs
+++ day22/soln.rs
@@ -11,22 +11,40 @@ use std::collections::{HashSet, BinaryHeap};
 extern crate aoc;
 extern crate regex;
 
+/// Turn on to show board for part 2.
+const SHOW: bool = false;
+
+/// Four cardinal directions.
+static DIRNS: [(isize, isize);4] = [(1, 0), (0, 1), (-1, 0), (0, -1)];
+
+
+/// Disk usage per node.
 #[derive(Clone, Debug)]
 struct Usage {
+    /// In-use.
     used: usize,
+    /// Free to use.
     avail: usize
 }
 
+/// The game state is just the position of the
+/// target data and blank.
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 struct State {
+    /// Position of target data.
     goal_data: (usize, usize),
+    /// Position of blank.
     blank: (usize, usize),
 }
 
+/// Node in A\* search.
 #[derive(Clone, Debug)]
 struct PQElem {
+    /// Cost so far.
     cost: usize,
+    /// Total heuristic cost.
     fcost: usize,
+    /// Actual state.
     state: State
 }
 
@@ -45,6 +63,8 @@ impl PartialOrd for PQElem {
 }
 
 impl Ord for PQElem {
+    /// Break ties in ordering by smallest heuristic cost
+    /// using largest actual cost.
     fn cmp(&self, other: &PQElem) -> Ordering {
         match other.fcost.cmp(&self.fcost) {
             Ordering::Equal =>
@@ -54,6 +74,7 @@ impl Ord for PQElem {
     }
 }
 
+/// Try to match given regex, returning submatches if successful.
 fn try_pat(pat: &regex::Regex, target: &str) -> Option<Vec<String>> {
     match pat.captures(target) {
         Some(parts) =>  {
@@ -69,14 +90,20 @@ fn try_pat(pat: &regex::Regex, target: &str) -> Option<Vec<String>> {
     }
 }
 
+/// Read usages from `stdin` and return a vector with
+/// position and usage information.
 fn read_usages() -> Vec<(usize, usize, Usage)> {
+    // Set up state.
     let usage_pat =
         regex::Regex::new(
             r"^/dev/grid/node-x(\d+)-y(\d+) *\d+T *(\d+)T *(\d+)T *\d+%$")
             .expect("could not compile usage pattern");
     let mut usages = Vec::new();
+
+    // Process each line.
     for target in aoc::input_lines() {
         if let Some(args) = try_pat(&usage_pat, &target) {
+            assert!(args.len() == 4);
             let mut argv = [0usize;4];
             for i in 0..4 {
                 argv[i] = args[i].parse::<usize>().unwrap();
@@ -88,21 +115,36 @@ fn read_usages() -> Vec<(usize, usize, Usage)> {
     usages
 }
 
+/// Use the obvious quadratic algorithm to test each pair
+/// For viability. Pairs are directed, so no need to divide
+/// by two or anything like that.
 fn count_viable_pairs(umap: &Vec<Vec<Usage>>) -> usize {
+    // Set up state.
     let len_x = umap.len();
     let len_y = umap[0].len();
     let mut viable_pairs = 0;
+
+    // Loop over source coordinates.
     for x0 in 0..len_x {
     for y0 in 0..len_y {
+        // Get source use for later.
         let used_a = umap[x0][y0].used;
+
+        // Never pull from empty disc.
         if used_a == 0 {
             continue;
         };
+
+        // Loop over destination coordinates.
         for x1 in 0..len_x {
         for y1 in 0..len_y {
+            // Source and dest must be different.
             if x1 == x0 && y1 == y0 {
                 continue;
             };
+
+            // Get dest space, and bump count if source
+            // fits.
             let avail_b = umap[x1][y1].avail;
             if avail_b >= used_a {
                 viable_pairs += 1;
@@ -112,13 +154,23 @@ fn count_viable_pairs(umap: &Vec<Vec<Usage>>) -> usize {
     viable_pairs
 }
 
+/// Heuristically transform the given resource usage map
+/// into a sliding tile puzzle using the methodology
+/// detailed in the description of part 2. Return
+/// a starting state and a set of coordinates that
+/// are "wall squares".
 fn start_info(umap: &Vec<Vec<Usage>>) -> (State, HashSet<(usize, usize)>) {
+    // Set up state.
     let len_x = umap.len();
     let len_y = umap[0].len();
     let mut blank: Option<(usize, usize)> = None;
     let mut tiles: HashSet<(usize, usize)> = HashSet::new();
+
+    // Loop over source coordinates.
     for x in 0..len_x {
     for y in 0..len_y {
+        // An empty disc represents the blank. There can
+        // be only one.
         if umap[x][y].used == 0 {
             if let Some(_) = blank {
                 panic!("two blanks");
@@ -126,38 +178,48 @@ fn start_info(umap: &Vec<Vec<Usage>>) -> (State, HashSet<(usize, usize)>) {
             blank = Some((x, y));
             continue;
         };
+
+        // Heuristically decide whether the given square is
+        // a tile or a wall. Block because early return.
         let is_tile = || {
-            for dx in -1..2isize {
-            for dy in -1..2isize {
-                if dx == 0 && dy == 0 {
-                    continue;
-                };
+            // Loop over immediate neighbors.
+            for &(dx, dy) in DIRNS.iter() {
+                // Compute neighbor coordinate.
                 let x0 = x as isize + dx;
                 let y0 = y as isize + dy;
                 if x0 < 0 || x0 >= len_x as isize
                    || y0 < 0 || y0 >= len_y as isize {
                     continue;
                 };
+
+                // Heuristic: If this neighbor could not
+                // accept the data if empty, it is a wall.
                 let u = &umap[x0 as usize][y0 as usize];
                 if umap[x][y].used > u.used + u.avail {
                     return false;
                 }
-            }};
+            };
             true
         };
+
+        // Run the block.
         if is_tile() {
             tiles.insert((x, y));
         };
     }};
+
+    // Check on the blank.
     let blank =
         match blank {
             Some(xy) => xy,
             None => panic!("no blank found")
         };
-    (State { goal_data: (len_x - 1, 0),
-             blank: blank }, tiles)
+
+    // Return the new state and tiles.
+    (State { goal_data: (len_x - 1, 0), blank: blank }, tiles)
 }
 
+// Display the part 2 map for debugging.
 fn print_map(s: &State, tiles: &HashSet<(usize, usize)>,
              len_x: usize, len_y: usize) {
     for y in 0..len_y {
@@ -176,19 +238,23 @@ fn print_map(s: &State, tiles: &HashSet<(usize, usize)>,
     };
 }
 
+// Manhattan distance between coordinates.
 fn mh_dist(p1: (usize, usize), p2: (usize, usize)) -> usize {
     (p1.0 as isize - p2.0 as isize).abs() as usize +
     (p1.1 as isize - p2.1 as isize).abs() as usize
 }
 
+// An admissible heuristic cost for a state is the cost of
+// getting the blank next to the data plus the cost of
+// getting the data to the goal.
 fn hcost(state: &State) -> usize {
     max(0,
         mh_dist(state.goal_data, state.blank) +
         mh_dist(state.goal_data, (0,0)))
 }
 
+// Generic A\* search.
 fn a_star(start: &State, tiles: &HashSet<(usize, usize)>) -> Option<usize> {
-    let dirns = [(1, 0), (0, 1), (-1, 0), (0, -1)];
     let mut stop_list = HashSet::new();
     let mut pq = BinaryHeap::new();
     pq.push(PQElem{ state: start.clone(), cost: 0, fcost: hcost(start) });
@@ -202,7 +268,7 @@ fn a_star(start: &State, tiles: &HashSet<(usize, usize)>) -> Option<usize> {
                     false => { continue; },
                     true => {
                         let (x, y) = state.blank;
-                        for &(dx, dy) in dirns.iter() {
+                        for &(dx, dy) in DIRNS.iter() {
                             let next_x = x as isize + dx;
                             let next_y = y as isize + dy;
                             let next_blank = (next_x as usize,
@@ -233,10 +299,15 @@ fn a_star(start: &State, tiles: &HashSet<(usize, usize)>) -> Option<usize> {
     };
 }
 
+// Do the appropriate search through the disc array.
 pub fn main() {
     let part = aoc::get_part();
     assert!(part == 1 || part == 2);
+
+    // Process input.
     let usages = read_usages();
+
+    // Find maximum x, y and total use.
     let mut max_x = 0;
     let mut max_y = 0;
     let mut max_t = 0;
@@ -247,6 +318,8 @@ pub fn main() {
     };
     let len_x = max_x + 1;
     let len_y = max_y + 1;
+
+    // Build the map.
     let mut umap: Vec<Vec<Usage>> = Vec::new();
     let mut row = Vec::new();
     row.resize(len_y, Usage{used: 0, avail: 0});
@@ -254,11 +327,16 @@ pub fn main() {
     for (x, y, u) in usages.iter().cloned() {
         umap[x][y] = u;
     };
+
     if part == 1 {
+        // Just count the pairs.
         println!("{}", count_viable_pairs(&umap));
     } else {
+        // Solve the sliding tile puzzle.
         let (start, tiles) = start_info(&umap);
-        print_map(&start, &tiles, len_x, len_y);
+        if SHOW {
+            print_map(&start, &tiles, len_x, len_y);
+        };
         if let Some(g) = a_star(&start, &tiles) {
             println!("{}", g);
         } else {
