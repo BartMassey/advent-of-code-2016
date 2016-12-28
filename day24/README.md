f31fbf06dae9b0b952a21eb291b47a0c93c2e6fc
Bart Massey <bart@cs.pdx.edu>
2016-12-27 18:01:31 -0800

    cleaned day 24

--- day24/README.md
+++ day24/README.md
@@ -7,9 +7,20 @@ placed just below 100 on the leaderboard on both parts,
 which is disappointing since it was probably my last chance
 to get on the leaderboard.
 
-There are A\* things that could be done here, but at 0.33s
-for part 1 and 0.65s for part 2 it hardly seems worth the
-considerable hassle.
+There is probably a reasonable admissible heuristic to be
+found for A\*, but at 0.33s for part 1 and 0.65s for part 2
+it hardly seems worth the considerable hassle of figuring it
+out.
+
+## Usage
+
+Part 1:
+
+        cargo run --release 1 <input.txt
+
+Part 2:
+
+        cargo run --release 2 <input.txt
 
 ---
 
--- day24/soln.rs
+++ day24/soln.rs
@@ -3,24 +3,31 @@
 // Please see the file COPYING in this distribution
 // for license terms.
 
-// Advent of Code Day 24.
+//! Advent of Code Day 24.
 
 use std::cmp::*;
 use std::collections::{HashSet, BTreeSet, BinaryHeap, HashMap};
 
 extern crate aoc;
 
+/// Type of 2D points.
 type Point = (usize, usize);
 
+/// Problem state.
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 struct State {
+    /// Current position.
     posn: Point,
+    /// Set of previously-visited positions.
     visited: BTreeSet<u32>
 }
 
+/// A\* / Dijkstra node.
 #[derive(Clone, Debug)]
 struct PQElem {
+    /// Cost to get to this state.
     cost: usize,
+    /// Problem state.
     state: State
 }
 
@@ -44,14 +51,19 @@ impl Ord for PQElem {
     }
 }
 
+/// Run the search, optionally returning the robot to its
+/// starting point.
 pub fn main() {
     let part = aoc::get_part();
+
+    // Set up state.
     let mut open: HashSet<Point> = HashSet::new();
     let mut goals: HashMap<Point, u32> = HashMap::new();
     let mut goal_labels: BTreeSet<u32> = BTreeSet::new();
     let mut maybe_start: Option<Point> = None;
     let mut x = 0;
     let mut y = 0;
+    // Process the map.
     for row in aoc::input_lines() {
         for c in row.chars() {
             match c {
@@ -60,6 +72,7 @@ pub fn main() {
                 dc if dc.is_digit(10) => {
                     match dc.to_digit(10).unwrap() {
                         0 => {
+                            // Location 0 is special.
                             maybe_start = Some((x, y));
                             open.insert((x, y));
                         },
@@ -84,32 +97,44 @@ pub fn main() {
         Some(p) => p,
         None => panic!("no start position found")
     };
-    let start = State{ posn: start_posn, visited: BTreeSet::new() };
     let dirns = [(1, 0), (0, 1), (-1, 0), (0, -1)];
+
+    // Run the Dijkstra (A\*) search.
+    let start = State{ posn: start_posn, visited: BTreeSet::new() };
     let mut stop_list: HashSet<State> = HashSet::new();
     let mut pq: BinaryHeap<PQElem> = BinaryHeap::new();
     pq.push(PQElem{ state: start, cost: 0 } );
     loop {
         match pq.pop() {
             Some(PQElem{cost: g, state}) => {
+                // Stop when everything has been visited,
+                // but in part 2 only when we then get home.
                 if state.visited.len() == goals_len {
                     if part == 1 || state.posn == start_posn {
                         println!("{}", g);
                         return;
                     }
                 };
+
+                // Process all the neighbors.
                 match stop_list.insert(state.clone()) {
                     false => { continue; },
                     true => {
+                        // Search each neighbor in turn.
                         let (x, y) = state.posn;
                         for &(dx, dy) in dirns.iter() {
+                            // Find the neighbor location.
                             let next_x = x as isize + dx;
                             let next_y = y as isize + dy;
                             let next_posn = (next_x as usize,
                                               next_y as usize);
+
+                            // Can't go through walls.
                             if !open.contains(&next_posn) {
                                 continue;
                             };
+
+                            // Add to the visited list if needed.
                             let mut next_visited = state.visited.clone();
                             match goals.get(&next_posn) {
                                 None => (),
@@ -117,6 +142,8 @@ pub fn main() {
                                     next_visited.insert(*d);
                                 }
                             };
+
+                            // Push the neighbor state.
                             let next_state = State{ posn: next_posn,
                                                     visited: next_visited };
                             pq.push( PQElem{ state: next_state,
@@ -126,8 +153,7 @@ pub fn main() {
                 }
             },
             None => {
-                println!("no solution");
-                return;
+                panic!("no solution");
             }
         }
     }
